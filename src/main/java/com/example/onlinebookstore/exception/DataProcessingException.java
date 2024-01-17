c45d3853470ee2f8cf55db6607c8a9f6ebc4a07f
Artem Peiev <slaybrute@gmail.com>
2024-01-17 15:27:42 +0200

    add book model + dao + servcie

---
+++ src/main/java/com/example/onlinebookstore/exception/DataProcessingException.java
@@ -0,0 +1,11 @@
+package com.example.onlinebookstore.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException(String message, Exception e) {
+        super(message, e);
+    }
+}
